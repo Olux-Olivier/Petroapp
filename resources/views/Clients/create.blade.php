da8e62fb8275f17aad2d3d6acc2f3d78b20abcb2
Olivier Kasongo <olivierkasongo539@gmail.com>
2024-04-10 10:07:27 +0100

    mangager migration

---
+++ resources/views/Clients/create.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un client</title>
+</head>
+<body>
+    <h1>Ajout Client</h1>
+    <div>
+        <form action="" method="post">
+            @csrf
+            <label for="">Nom</label>
+            <input type="text" name="nom" >
+
+            <label for="">Type client</label>
+           <select name="typeclient" id="">
+                <option value="entreprise">Entreprise</option>
+                <option value="particulier">Particulier</option>
+           </select>
+
+           <label for="">Email</label>
+           <input type="email" name="email">
+
+           <label for="">Adresse</label>
+           <input type="text" name="adresse">
+
+           <input type="submit" value="Ajouter">
+        </form>
+    </div>
+</body>
+</html>

a5e385351247793ce28249e969133a9d321b7bed
Olivier Kasongo <olivierkasongo539@gmail.com>
2024-04-10 16:18:25 +0100

    modif manager migration

--- database/migrations/2024_04_10_084558_managers.php
+++ database/migrations/2024_04_10_084558_managers.php
@@ -12,6 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create('managers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('postnom');
+            $table->string('prenom');
+            $table->string('email');
+            $table->string('mdp');
+            $table->timestamps();
+        });
     }
 
     /**

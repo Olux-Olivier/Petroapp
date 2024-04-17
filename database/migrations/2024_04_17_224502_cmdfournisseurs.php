16b548ba01be6369fcdbf692a638d01011bf9f66
Olivier Kasongo <olivierkasongo539@gmail.com>
2024-04-17 23:53:40 +0100

    add migration cmdfrsss

--- database/migrations/2024_04_17_224502_cmdfournisseurs.php
+++ database/migrations/2024_04_17_224502_cmdfournisseurs.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         //
-        Schema::create('fournisseurs', function (Blueprint $table) {
+        Schema::create('cmdfournisseurs', function (Blueprint $table) {
             $table->id();
             $table->integer('qte');
             $table->string('email');
@@ -29,5 +29,6 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::dropIfExists('cmdfournisseurs');
     }
 };
